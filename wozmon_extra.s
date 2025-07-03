2e40c294db5b5ffb92b09ceddd295fe5d1dfba8c
Tore L. Tangen <Aetesaki@users.noreply.github.com>
2025-07-03 22:09:41 +0200

    Update wozmon_extra.s
    Add comments and store x on stack

--- wozmon_extra.s
+++ wozmon_extra.s
@@ -5,16 +5,20 @@
 wozmonlcdmessage: .asciiz "Wozmon running..."
 
 WOZMONLCD:
+  ; Print message to LCD panel
+  ; Modifies: x
   jsr ECHO     ; echo \
-  
+
+  phx
   ldx #0
-wozmonlcdprint:
+wozmonlcdprint: ; Loop through message and print each character
   lda wozmonlcdmessage,x
   beq wozmonlcd_END
   jsr lcd_print_char
   inx
   jmp wozmonlcdprint
 wozmonlcd_END:
+  plx
   rts
 
 WOZMONCRLF: ; Outputs LF in addition to CR
